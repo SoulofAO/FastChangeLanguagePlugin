4d1414f63532bed10d63c502d72be6c7670d3ecd
SoulofAO <SoulofAO@mail.com>
2024-03-31 11:13:31 +0400

    Update

---
+++ ChangeLanguage/Intermediate/Build/Win64/UnrealEditor/Inc/ChangeLanguage/UHT/ChangeLanguage.generated.h
@@ -0,0 +1,58 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "ChangeLanguage.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef CHANGELANGUAGE_ChangeLanguage_generated_h
+#error "ChangeLanguage.generated.h already included, missing '#pragma once' in ChangeLanguage.h"
+#endif
+#define CHANGELANGUAGE_ChangeLanguage_generated_h
+
+#define FID_PluginPackage_ChangeLanguage_HostProject_Plugins_ChangeLanguage_Source_ChangeLanguage_Public_ChangeLanguage_h_18_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUChangeLanguageDeveloperSettings(); \
+	friend struct Z_Construct_UClass_UChangeLanguageDeveloperSettings_Statics; \
+public: \
+	DECLARE_CLASS(UChangeLanguageDeveloperSettings, UDeveloperSettings, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/ChangeLanguage"), NO_API) \
+	DECLARE_SERIALIZER(UChangeLanguageDeveloperSettings) \
+	static const TCHAR* StaticConfigName() {return TEXT("EditorPerProjectUserSettings");} \
+
+
+
+#define FID_PluginPackage_ChangeLanguage_HostProject_Plugins_ChangeLanguage_Source_ChangeLanguage_Public_ChangeLanguage_h_18_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UChangeLanguageDeveloperSettings(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	UChangeLanguageDeveloperSettings(UChangeLanguageDeveloperSettings&&); \
+	UChangeLanguageDeveloperSettings(const UChangeLanguageDeveloperSettings&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UChangeLanguageDeveloperSettings); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UChangeLanguageDeveloperSettings); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UChangeLanguageDeveloperSettings) \
+	NO_API virtual ~UChangeLanguageDeveloperSettings();
+
+
+#define FID_PluginPackage_ChangeLanguage_HostProject_Plugins_ChangeLanguage_Source_ChangeLanguage_Public_ChangeLanguage_h_15_PROLOG
+#define FID_PluginPackage_ChangeLanguage_HostProject_Plugins_ChangeLanguage_Source_ChangeLanguage_Public_ChangeLanguage_h_18_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_PluginPackage_ChangeLanguage_HostProject_Plugins_ChangeLanguage_Source_ChangeLanguage_Public_ChangeLanguage_h_18_INCLASS_NO_PURE_DECLS \
+	FID_PluginPackage_ChangeLanguage_HostProject_Plugins_ChangeLanguage_Source_ChangeLanguage_Public_ChangeLanguage_h_18_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> CHANGELANGUAGE_API UClass* StaticClass<class UChangeLanguageDeveloperSettings>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_PluginPackage_ChangeLanguage_HostProject_Plugins_ChangeLanguage_Source_ChangeLanguage_Public_ChangeLanguage_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
